51d83eb47cacba8a6bfb7327ff9b8bf84911af7f
Philip Steiner <redveedub@gmail.com>
2023-06-30 16:19:52 -0700

    checkpoint

---
+++ src/game.rs
@@ -0,0 +1,104 @@
+use crate::prelude::*;
+
+/*
+Notes on the original Atari Pong cabinet game play from these videos :
+https://youtu.be/_MxlPkD7mEw?t=909
+https://youtu.be/ut6Rh-rmGAo?t=1204
+
+    Idle:
+        - display last score, ball bounces randomly around screen, no paddles or
+        sound effects
+
+    Playing:
+        - game starts when a coin is inserted
+        - After the coin is inserted, the screen 'flips' as the game initializes
+        - the scores are reset to '0'
+        - after two or three seconds, the ball is served from the centre line
+
+    Over:
+        - first player to 11
+
+    Sounds:
+        - low-pitched 'boop' when ball rebounds from top or bottom edge
+        - higher-pitched 'bip' when paddle intercepts ball
+        - longer, higher-pitched 'buuuzzz' when ball scores
+
+        - volley speeds up the longer the ball is in play
+        - three speed levels
+*/
+enum Mode {
+    Idle,
+    Playing,
+    Over,
+}
+
+pub struct Paddle {
+    pub x: i32,
+    pub y: i32,
+    pub height: u32,
+    pub width: u32,
+}
+
+impl Paddle {
+    pub fn new(x: i32, y: i32, height: u32, width: u32) -> Paddle {
+        Paddle {
+            x,
+            y,
+            height,
+            width,
+        }
+    }
+
+    pub fn move_up(&self) {}
+    pub fn move_down(&self) {}
+}
+
+pub struct Ball {
+    pub x: i32,
+    pub y: i32,
+    pub size: u32,
+}
+
+impl Ball {
+    pub fn new(x: i32, y: i32, size: u32) -> Ball {
+        Ball { x, y, size }
+    }
+
+    pub fn update() {}
+}
+
+pub struct Game {
+    mode: Mode,
+    pub player: Paddle,
+    pub opponent: Paddle,
+    pub ball: Ball,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            mode: Mode::Idle,
+            player: Paddle::new(
+                SCREEN_MARGIN,
+                RACKET_CENTRE,
+                RACKET_HEIGHT,
+                RACKET_WIDTH,
+            ),
+            opponent: Paddle::new(
+                WINDOW_WIDTH as i32 - SCREEN_MARGIN - RACKET_WIDTH as i32,
+                RACKET_CENTRE,
+                RACKET_HEIGHT,
+                RACKET_WIDTH,
+            ),
+            ball: Ball::new(
+                WINDOW_WIDTH as i32 / 2,
+                WINDOW_HEIGHT as i32 / 2,
+                BALL_SIZE,
+            ),
+        }
+    }
+
+    pub fn update(&self) {}
+
+    pub fn start(&self) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,73 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
+mod game;
+mod ui;
+
+mod prelude {
+    pub use crate::game::Game;
+    pub use crate::ui::*;
+    pub use sdl2::event::Event;
+    pub use sdl2::keyboard::Keycode;
+    pub use sdl2::pixels::Color;
+    pub use std::time::Duration;
+    pub use sdl2::render::Canvas;
+    pub use sdl2::video::Window;
+
+    pub const WINDOW_WIDTH: u32 = 800;
+    pub const WINDOW_HEIGHT: u32 = 600;
+    pub const SCREEN_MARGIN: i32 = 10;
+    pub const RACKET_HEIGHT: u32 = WINDOW_HEIGHT / 8;
+    pub const RACKET_WIDTH: u32 = 10;
+    pub const RACKET_CENTRE: i32 =
+        WINDOW_HEIGHT as i32 / 2 - RACKET_HEIGHT as i32 / 2;
+    pub const BALL_SIZE: u32 = 10;
+}
+
+use prelude::*;
 
 pub fn main() -> Result<(), String> {
+
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("rust-pong", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.set_draw_color(Color::BLACK);
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
 
+    let mut game = Game::new();
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
+                Event::Quit { .. } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
                     ..
-                } => break 'running,
+                } => {
+                    game.start();
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => game.player.move_up(),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => game.player.move_down(),
                 _ => {}
             }
         }
 
-        canvas.clear();
-        canvas.present();
+        game.update();
+        draw_game(&mut canvas, &game);
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
     }
-
     Ok(())
 }
