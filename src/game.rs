9987bbb01d8f0bd3622474f911ab14fbe87d65f7
Philip Steiner <redveedub@gmail.com>
2023-07-07 11:59:41 -0700

    update Ball

--- src/game.rs
+++ src/game.rs
@@ -65,16 +65,23 @@ impl Paddle {
 pub struct Ball {
     pub x: i32,
     pub y: i32,
+    pub dx: i32,
+    pub dy: i32,
     pub size: u32,
 }
 
 impl Ball {
     pub fn new(x: i32, y: i32, size: u32) -> Ball {
-        Ball { x, y, size }
+        Ball { x, y, dx: 3, dy: 3, size }
     }
 
-    pub fn update(&self) {
-        
+    pub fn update(&mut self) {
+        self.x += self.dx;
+        self.y += self.dy;
+
+        if self.x < 0 {
+            
+        }
     }
 }
 
